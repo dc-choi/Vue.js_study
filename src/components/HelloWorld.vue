de7d72a1ccd17d8b65af09ab27b96db710556986
dcchoi <cheek0805@naver.com>
2022-04-03 16:55:17 +0900

    vue 3 튜토리얼: 템플릿 진행중

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,10 +1,19 @@
 <template>
   <div class="hello">
+    <!--
+      문자열 보간법 {{}}
+      이중 중괄호는 데이터를 HTML이 아닌 일반 텍스트로 해석합니다.
+      웹사이트에서 임의의 HTML을 동적으로 렌더링하면 XSS 취약점으로 쉽게 이어질 수 있고 이는 매우 위험할 소지가 있습니다.
+      HTML 보간법은 반드시 신뢰할 수 있는 콘텐츠에서만 사용하고 사용자가 제공한 콘텐츠에서는 절대 사용하면 안 됩니다.
+      이중 중괄호는 HTML 속성에 사용할 수 없습니다. 대신 v-bind 디렉티브를 사용하세요.
+    -->
     <div>
       {{ count }}
     </div>
     <div>
       <span v-bind:title="messege">올려두면 정보를 띄웁니다.</span>
+      <div v-bind:id="dynamicId">qwer</div>
+      <button v-bind:disabled="isButtonDisabled">Button</button>
     </div>
     <div>
       {{ name }}
@@ -27,6 +36,17 @@
     <div>
       {{ msg }}
     </div>
+    <div>
+      <span v-once>결코 변하지 않을 것입니다: {{ data }}</span>
+    </div>
+    <div>
+      <p>v-html 디렉티브 사용: <span v-html="rawHtml"></span></p>
+    </div>
+    <!-- {{}} 안에 JS 표현식으로 사용 가능 -->
+    <div>
+      {{ number + 1 }}
+      {{ ok ? "YES" : "NO" }}
+    </div>
   </div>
 </template>
 
@@ -48,6 +68,11 @@ export default {
         { text: "Learn Vue" },
         { text: "Build something awesome" },
       ],
+      rawHtml: `<span style="color: red">This should be red.</span>`,
+      dynamicId: `hi`,
+      isButtonDisabled: true, // null, undefined
+      number: 1,
+      ok: true,
     };
   },
   methods: {
@@ -65,7 +90,7 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-div {
-  scroll-margin: unset;
+.hello {
+  margin: auto;
 }
 </style>
